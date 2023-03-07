09f0236ba5845ae7a8cfa709bbfbc672c6e6c22a
Otavio Leomar Angelin Zwirtes <109840981+OtavioLeomar@users.noreply.github.com>
2023-03-06 23:28:37 -0300

    Commit inicial

---
+++ swetetAlert.js
@@ -0,0 +1,204 @@
+
+function texto1() {
+  Swal.fire({
+    title: 'Um precipício sem fim...',
+    html:
+      '"E aqui eu vi o quanto a vida era cruel... A data? Não consigo me lembrar, mas sei o quanto aquele dia foi doloroso. A chegada ao fim de um relacionamento tão duradouro me fez cair em queda livre num precipício sem fim. Uma vez caindo, não consegui achar uma maneira de sair daquela situação e exergo tudo caindo lentamente junto a mim. Os planos, sonhos e objetivos de vida iam se perdendo enquanto eu continuava a cair mais e mais. Talvez eu não tivesse notado e com toda certeza demoraria um tempo até perceber, mas estava vivendo uma dependecia emocional, onde tive um apego tão grande ( Afinal eram muitos anos juntos) a uma pessoa que tudo o que eu fazia girava em torno dela. E dali para frente sabia que os outros dias seriam os piores que já passei em toda minha vida..."',
+    footer: '<em>Otávio Zwirtes - 10/01/2023</em>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+
+  })
+}
+
+function texto2() {
+  Swal.fire({
+    title: 'Deus está morto e nós o matamos...',
+    imageUrl: 'midias/nietzsche.jpg',
+    imageWidth: 200,
+    imageHeight: 100,
+    imageAlt: 'Custom image',
+    html:
+      '"O homem louco se lançou para o meio deles e trespassou-os com seu olhar. ‘Para onde foi Deus’, gritou ele, ‘já lhes direi! Nós o matamos – vocês e eu. Somos todos seus assassinos! Mas como fizemos isso? Como conseguimos beber inteiramente o mar? Quem nos deu a esponja para apagar o horizonte? Que fizemos nós, ao desatar a terra do seu sol? Para onde se move agora? Para onde nos movemos nós? Para longe de todos os sóis? Não caímos continuamente? Para trás, para os lados, para a frente, em todas as direções? Existem ainda ’em cima’ e ’embaixo’? Não vagamos como que através de um nada infinito? Não sentimos na pele o sopro do vácuo? Não se tornou ele mais frio? Não anoitece eternamente? Não temos que acender lanternas de manhã?”',
+    footer: '<em> Nietzsche, A Gaia Ciência, §125</em>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+
+  })
+}
+
+function texto3() {
+  Swal.fire({
+    title: '<bold>Amor Fati</bold>',
+    html:
+      '“Amor fati seria então o supremo ato de amor próprio. Você olha pra sua vida e reconhece que pra ser quem você é hoje, tudo precisou acontecer do jeito que aconteceu. E sim, algumas coisas foram tremendamente injustas, outras te deixam acordado de noite pensando: “e se fosse diferente?”, mas no fim cada uma dessas dores te ensinou alguma coisa… E por essa razão, elas merecem o seu amor, não porque elas foram boas, mas porque elas são parte de você. E talvez você nem esteja feliz com quem você é hoje, mas ai é que tá, graças ao que te aconteceu, agora você sabe que precisa mudar e também sabe que não vai ser fácil.”' +
+      '<br><br><em>https://www.youtube.com/watch?v=z4plH2K2uHY</em>',
+    footer: '<em>Ludo Viajante</em>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+
+  })
+}
+
+function texto4() {
+  Swal.fire({
+    title: 'Vazio',
+    imageUrl: 'midias/vazio2.jpg',
+    imageWidth: 200,
+    imageHeight: 150,
+    imageAlt: 'Custom image',
+    html:
+      '"Querido, maldito, infinito vazio. Depois de muito chorar, depois de muito sofrer, hoje sou eu que procuro você."',
+    footer: '<em>Ludo Viajante</em>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+}
+
+function texto5() {
+  Swal.fire({
+    title: 'Noite Chuvosa',
+    width: '70em',
+    padding: '10px',
+    text: '"Já são 3 horas da manhã e a chuva começa a cair lentamente. Barulhos de trovões vão tomando conta do silêncio que envolve meu quarto. Acordo com o coração acelerado e rapidamente procuro seus braços para me acolher, e para minha surpresa? Você não está mais lá...Num piscar de olhos duas pessoas abrem a porta do meu quarto. É a solidão e a tristeza que vieram me fazer companhia, nessa noite chuvosa. Sentado ao pé da cama e tomado por esses sentimentos, iniciamos uma conversa para tentarmos achar explicações sobre tudo isso.  - O que você está sentindo? - pergunta a tristeza. - Sinto um grande vazio em meu peito, é como se naquele dia uma parte de mim tivesse ficado com ela. - respondo-lhe rapidamente. - Você não está sozinho, nós estamos aqui! - responde a solidão.Inconformado deito-me novamente na cama e fecho os olhos para tentar descansar.A Cada minuto que passa os trovões vão ficando cada vez mais altos! Ela sabia que trovões e tempestades sempre me trouxeram desconforto e cada vez que começava uma tempestade ela me abraçava forte e dizia para eu me acalmar. Nesse dia foi diferente... De repente escuto uma voz sussurrando em meu ouvido. A tristeza agarrada da solidão e agora ao vazio me acordam novamente. - Cadê todas aquelas promessas? Cadê as juras de amor? Cadê todo aquele eu teu amo? Cadê a pessoa que jurou não soltar sua mão? Elas me sussurravam. Agora tomado pela ansiedade e o pânico que batem em meu peito, me vejo a beira do precipício e prestes a pular, quando sinto alguém segurar minha mão. É você...Senti o toque das suas mãos em meu resto e pelo resto daquela noite não tive mais a visita da tristeza, da solidão ou do vazio. Um sonho lindo onde de mãos dadas jurávamos não nos soltar independente da situação, invade minha mente e aquece meu coração. Palavras que pude ouvir nitidamente saindo da sua boca me trazem a lembrança e conforto do seu abraço nessa noite tempestuosa. Passadas algumas horas o despertador começa a tocar e logo me acordo. Um pouco confuso sobre o que havia acontecido na noite anterior, busco algo para tentar me conformar de que tudo não passou de um sonho, quando de repente minha mãe adentra ao quarto e pergunta: - Com quem estava conversando ontem à noite? Ouvi umas conversas e parecia que tinha mais gente no quarto. Rapidamente me retorna a memória da visita que tive da tristeza, da solidão e do vazio. E se no fim de tudo isso a presença delas tiverem sido reais, que o sonho mais lindo que tive segurando suas mãos também fossem reais Torço para que a conexão e o calor que senti em meu coração, também tenha sentido no seu. A noite chegara novamente e esperarei com uma boa xicara de café a presença das minhas velhas amigas, e talvez por sorte do destino a visita não seja delas, mas seja sua."',
+    grow: 'row',
+    footer: '<em>Otávio Zwirtes 11/10/2022</em>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+}
+
+function texto6() {
+  Swal.fire({
+    title: 'Viva a vida...',
+    html:
+      '<iframe width="250" height="400" src="https://www.youtube.com/embed/yR9VrwJC-qA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>' +
+      '<br><br>Um video curto que me ensinou a entender um pouco sobre como devemos viver intensamente cada segundo da nossa vida.',
+    footer: '@Tiagoapresenta',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+}
+
+function texto7() {
+  Swal.fire({
+    title: 'Descobrimento',
+    width:'350px',
+    html:
+      '"Descobri hoje um grande poder oculto;' +
+      ' <br>Um dom de fênix para renascer das minhas próprias cinzas,' +
+      ' <br>Descobri que a estrada não acabou, ela só tomou um desvio...' +
+      ' <br>“No meio do caminho tinha uma pedra”' +
+      ' <br>Como em todo caminho tem uma perda' +
+      ' <br>Eu dei o melhor de mim, lutei com as armas que sabia lutar' +
+      ' <br>Fui derrotado, eu assumo.' +
+      ' <br>Apenas FUI.Não sou.Não serei.' +
+      ' <br>E agora?' +
+      ' <br>Levantarei de novo, quantas vezes for preciso' +
+      ' <br>Não usarei ninguém como espelho' +
+      ' <br>Quero chegar à frente e ver minha própria imagem' +
+      ' <br>O reflexo do que sou, do que fui e do que serei....' +
+      ' <br>Como já disseram: “ Quem sempre vence perde a glória de chorar”.' +
+      ' <br>O que seriam as grandes vitórias sem os grandes fracassos?' +
+      ' <br>Etapas...Ciclos...' +
+      ' <br>Eu sou o meu próprio motor de ignição' +
+      ' <br>E cabe a mim e a ninguém mais a escolha' +
+      ' <br>De ficar triste e ganhar pena' +
+      ' <br>Ou ficar feliz e ganhar a própria glória...' +
+      ' <br>E eu escolhi tentar outra vez...' +
+      ' <br>Paciência...Sabedoria só se ganha com tempo.' +
+      ' <br>E o tempo?' +
+      ' <br>“O tempo não para”' +
+      ' <br>Se valeu apena?' +
+      ' <br>“Tudo vale apena se a alma não é pequena”' +
+      ' <br>“Que não seja infinito, mas que seja eterno enquanto dure”' +
+      ' <br>“Os meus sonhos dependem de mim”' +
+      ' <br>Usarei uma força que eu talvez nunca tenha' +
+      ' <br>E direi o que quiserem ouvir, aqui dentro de mim só eu sei' +
+      ' <br>Para o frio existe o calor' +
+      ' <br>E essa chama até então desconhecida não permitirá."' +
+      ' <br>Que o frio permaneça."',
+    footer: '<em>Jefferson Cavalcante</em>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+
+  })
+}
+
+function texto8() {
+  Swal.fire({
+    title: 'Outra Vida',
+    html:
+      'Talvez não seja nessa vida ainda' +
+      '<br>Mas você ainda vai ser a minha vida' +
+      '<br>Então a gente vai fugir pro mar' +
+      '<br>Eu vou pedir pra namorar' +
+      '<br>Você vai me dizer que vai pensar' +
+      '<br><br>Mas, no fim, vai deixar' +
+      '<br>Talvez não seja nessa vida ainda' +
+      '<br>Mas você ainda vai ser a minha vida' +
+      '<br>Sem ter mais mentiras pra me ver' +
+      '<br>Sem amor antigo pra esquecer' +
+      '<br>Sem os teus amigos pra esconder' +
+      '<br>Pode crer que tudo vai dar certo' +
+      '<br><br>Uê-baruiê-iô' +
+      '<br>Sou pescador, sonhador' +
+      '<br>Vou dizer pra Deus, nosso senhor' +
+      '<br>Que tu és o amor da minha vida' +
+      '<br>Pois não dá pra viver nessa vida morrendo de amor' +
+      '<br>Oi, io' +
+      '<br><br>Talvez não seja nessa vida ainda' +
+      '<br>Mas você ainda vai ser a minha vida' +
+      '<br>E uma abelhinha vai fazer o mel' +
+      '<br>Estrela dAlva vai cruzar no céu' +
+      '<br>O vento certo vai soprar no mar' +
+      '<br>Pode crer que tudo vai dar certo' +
+      '<br><br>Uê-baruiê-iô' +
+      '<br>Sou pescador, sonhador' +
+      '<br>Vou dizer pra Deus, nosso senhor' +
+      '<br>Que tu és o amor da minha vida' +
+      '<br>Pois não dá pra viver nessa vida morrendo de amor' +
+      '<br><br>Uê-baruiê-iô' +
+      '<br>Sou pescador, sonhador' +
+      '<br>Vou dizer pra Deus, nosso senhor' +
+      '<br>Que tu és o amor da minha vida' +
+      '<br>Pois não dá pra viver nessa vida morrendo de amor' +
+      '<br>Uê-baruiê-iô',
+    footer: '<em>Armandinho</em>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+
+  })
+}
